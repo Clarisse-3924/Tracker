03a900d18c7a87cbc886043c56b8aa9f71b08e85
Clarisse-3924 <cuwizeyimana3924@gmail.com>
2020-12-03 12:54:00 +0200

    testing if animal type can have new class

--- src/test/java/wildlifetracker/AnimalTypeTest.java
+++ src/test/java/wildlifetracker/AnimalTypeTest.java
@@ -1,8 +1,20 @@
 package wildlifetracker;
 
+import org.junit.Rule;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
+
 public class AnimalTypeTest {
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
+    @Test
+    public void animal_instantiatesCorrectly_true() {
+        AnimalType testanimal = new AnimalType("Cat", "canvore");
+        assertEquals(true, testanimal instanceof AnimalType);
+    }
+
 
 
 }
