98d66feb06c6cc67df10d46b278a4611877a5348
Clarisse-3924 <cuwizeyimana3924@gmail.com>
2020-12-03 09:51:46 +0200

    adding animal type class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,6 @@
 import spark.ModelAndView;
         import spark.template.handlebars.HandlebarsTemplateEngine;
-import wildlifetracker.AnimalType;
-import wildlifetracker.Location;
-import wildlifetracker.Ranger;
-import wildlifetracker.Sighting;
+import wildlifetracker.*;
 
 import java.util.ArrayList;
         import java.util.HashMap;
@@ -34,7 +31,7 @@ public class App {
         //ranger
         get("/create/ranger",(request, response) -> {
             Map<String,Object> model=new HashMap<String, Object>();
-            return new ModelAndView(model,"ranger-form.hbs");
+            return new ModelAndView(model,"rangerform.hbs");
         },new HandlebarsTemplateEngine());
 
         post("/create/ranger/new",(request, response) -> {
@@ -44,12 +41,12 @@ public class App {
             String phone_number=request.queryParams("phone");
             Ranger ranger=new Ranger(name,badge_number,phone_number);
             ranger.save();
-            return new ModelAndView(model,"ranger-form.hbs");
+            return new ModelAndView(model,"rangerform.hbs");
         },new HandlebarsTemplateEngine());
         get("/view/rangers",(request, response) -> {
             Map<String,Object> model=new HashMap<String, Object>();
             model.put("rangers",Ranger.all());
-            return new ModelAndView(model,"ranger-view.hbs");
+            return new ModelAndView(model,"rangerview.hbs");
         },new HandlebarsTemplateEngine());
         get("/view/ranger/sightings/:id",(request, response) -> {
             Map<String,Object> model=new HashMap<String, Object>();
@@ -64,11 +61,11 @@ public class App {
                 animals.add(animal_name);
                 types.add(animal_type);
             }
-            model.put("sighting",sightings);
-            model.put("animal",animals);
-            model.put("type",types);
-            model.put("ranger",Ranger.all());
-            return new ModelAndView(model,"ranger-view.hbs");
+            model.put("sightings",sightings);
+            model.put("animals",animals);
+            model.put("types",types);
+            model.put("rangers",Ranger.all());
+            return new ModelAndView(model,"rangerview.hbs");
         },new HandlebarsTemplateEngine());
 
 
@@ -78,7 +75,7 @@ public class App {
         //location
         get("/create/location",(request, response) -> {
             Map<String,Object> model=new HashMap<String, Object>();
-            return new ModelAndView(model,"location-form.hbs");
+            return new ModelAndView(model,"locationform.hbs");
         },new HandlebarsTemplateEngine());
 
         post("/create/location/new",(request, response) -> {
@@ -91,12 +88,12 @@ public class App {
                 System.out.println(e);
             }
 
-            return new ModelAndView(model,"location-form.hbs");
+            return new ModelAndView(model,"locationform.hbs");
         },new HandlebarsTemplateEngine());
         get("/view/locations",(request, response) -> {
             Map<String,Object> model=new HashMap<String, Object>();
             model.put("locations",Location.all());
-            return new ModelAndView(model,"location-view.hbs");
+            return new ModelAndView(model,"locationview.hbs");
         },new HandlebarsTemplateEngine());
 
         get("/view/location/sightings/:id",(request, response) -> {
@@ -115,15 +112,15 @@ public class App {
             model.put("sightings",sightings);
             model.put("animals",animals);
             model.put("types",types);
-            model.put("locations",Locations.all());
-            return new ModelAndView(model,"location-view.hbs");
+            model.put("locations",Location.all());
+            return new ModelAndView(model,"locationview.hbs");
         },new HandlebarsTemplateEngine());
 
 
         //animal
         get("/create/animal",(request, response) -> {
             Map<String,Object> model=new HashMap<String, Object>();
-            return new ModelAndView(model,"animal-form.hbs");
+            return new ModelAndView(model,"animalform.hbs");
         },new HandlebarsTemplateEngine());
 
         post("/create/animal/new",(request, response) -> {
@@ -136,50 +133,50 @@ public class App {
             System.out.println(age);
             String name=request.queryParams("name");
             System.out.println(name);
-            if(type.equals(EndangeredAnimals.ANIMAL_TYPE)){
-                EndangeredAnimals endangered=new EndangeredAnimals(name,EndangeredAnimals.ANIMAL_TYPE,health,age);
+            if(type.equals(Endangered.ANIMAL_TYPE)){
+                Endangered endangered=new Endangered(name,Endangered.ANIMAL_TYPE,health,age);
                 endangered.save();
             }
             else {
-                Animals animal=new Animals(name,Animals.ANIMAL_TYPE);
+                AnimalType animal=new AnimalType(name,AnimalType.ANIMAL_TYPE);
                 animal.save();
             }
 
-            return new ModelAndView(model,"animal-form.hbs");
+            return new ModelAndView(model,"animalform.hbs");
         },new HandlebarsTemplateEngine());
 
 
         get("/create/animal/endangered",(request, response) -> {
             Map<String,Object> model=new HashMap<String, Object>();
             List<String> health= new ArrayList<String>();
-            health.add(EndangeredAnimals.HEALTH_HEALTHY);
-            health.add(EndangeredAnimals.HEALTH_ILL);
-            health.add(EndangeredAnimals.HEALTH_OKAY);
+            health.add(Endangered.HEALTH_HEALTHY);
+            health.add(Endangered.HEALTH_ILL);
+            health.add(Endangered.HEALTH_OKAY);
             List<String> age= new ArrayList<String>();
-            age.add(EndangeredAnimals.AGE_ADULT);
-            age.add(EndangeredAnimals.AGE_NEWBORN);
-            age.add(EndangeredAnimals.AGE_YOUNG);
+            age.add(Endangered.AGE_ADULT);
+            age.add(Endangered.AGE_NEWBORN);
+            age.add(Endangered.AGE_YOUNG);
             model.put("health",health);
             model.put("age",age);
             String typeChosen="endangered";
             model.put("endangered",typeChosen);
-            return new ModelAndView(model,"animal-form.hbs");
+            return new ModelAndView(model,"animalform.hbs");
         },new HandlebarsTemplateEngine());
 
         get("/view/animals",(request, response) -> {
             Map<String,Object> model=new HashMap<String, Object>();
-            model.put("animals",Animals.all());
-            return new ModelAndView(model,"animal-view.hbs");
+            model.put("animals",AnimalType.all());
+            return new ModelAndView(model,"animalview.hbs");
         },new HandlebarsTemplateEngine());
 
 
         //sighting
         get("/create/sighting",(request, response) -> {
             Map<String,Object> model=new HashMap<String, Object>();
-            model.put("rangers",Rangers.all());
-            model.put("locations",Locations.all());
-            model.put("animals",Animals.all());
-            return new ModelAndView(model,"sighting-form.hbs");
+            model.put("rangers",Ranger.all());
+            model.put("locations",Location.all());
+            model.put("animals",AnimalType.all());
+            return new ModelAndView(model,"sighting.hbs");
         },new HandlebarsTemplateEngine());
 
         post("/create/sighting/new",(request, response) -> {
@@ -188,26 +185,26 @@ public class App {
             int ranger_id= Integer.parseInt(request.queryParams("ranger"));
             int animal_id= Integer.parseInt(request.queryParams("animal"));
 
-            Sightings sighting=new Sightings(location_id,ranger_id,animal_id);
+            Sighting sighting=new Sighting(location_id,ranger_id,animal_id);
             sighting.save();
-            return new ModelAndView(model,"sighting-form.hbs");
+            return new ModelAndView(model,"sighting.hbs");
         },new HandlebarsTemplateEngine());
 
         get("/view/sightings",(request, response) -> {
             Map<String,Object> model=new HashMap<String, Object>();
-            List<Sightings> sightings=Sightings.all();
+            List<Sighting> sightings=Sighting.all();
             ArrayList<String> animals=new ArrayList<String>();
             ArrayList<String> types=new ArrayList<String>();
-            for (Sightings sighting : sightings){
-                String animal_name=Animals.find(sighting.getAnimal_id()).getName();
-                String animal_type=Animals.find(sighting.getAnimal_id()).getType();
+            for (Sighting sighting : sightings){
+                String animal_name=AnimalType.find(sighting.getAnimal_id()).getName();
+                String animal_type=AnimalType.find(sighting.getAnimal_id()).getType();
                 animals.add(animal_name);
                 types.add(animal_type);
             }
             model.put("sightings",sightings);
             model.put("animals",animals);
             model.put("types",types);
-            return new ModelAndView(model,"sighting-view.hbs");
+            return new ModelAndView(model,"sightingview.hbs");
         },new HandlebarsTemplateEngine());
 
 
--- src/main/java/wildlifetracker/AnimalType.java
+++ src/main/java/wildlifetracker/AnimalType.java
@@ -1,8 +1,9 @@
-package wildlifetracker;
 
+import org.sql2o.Connection;
 import org.sql2o.Sql2oException;
+import wildlifetracker.DB;
+import wildlifetracker.Dbmanagement;
 
-import java.sql.Connection;
 import java.util.List;
 import java.util.Objects;
 
@@ -46,9 +47,9 @@ public class AnimalType implements Dbmanagement {
 
     public void save(){
         if(this.name.equals("")||this.type.equals("")||this.name.equals(null)||this.type.equals(null)){
-            throw new IllegalArgumentException("Please all field are required!!");
+            throw new IllegalArgumentException("Fields cannot be empty");
         }
-        try (Connection con=DB.sql2o.open()){
+        try (Connection con= DB.sql2o.open()){
 
 
             String sql ="INSERT INTO animals (name,type) VALUES (:name,:type)";
@@ -66,11 +67,11 @@ public class AnimalType implements Dbmanagement {
     public void update(int id,String type,String health,String age) {
         try (Connection con = DB.sql2o.open()) {
             if (type.equals("")) {
-                throw new IllegalArgumentException("field are required");
+                throw new IllegalArgumentException("All fields must be filled");
             }
             if (type == "endangered") {
                 if (health.equals("") || age.equals("")) {
-                    throw new IllegalArgumentException("field are required");
+                    throw new IllegalArgumentException("All fields must be filled");
                 }
                 String sql = "UPDATE animals SET type=:type,health=:health,age=:age WHERE id=:id";
                 con.createQuery(sql)
